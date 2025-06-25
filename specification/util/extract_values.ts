f33fb22efd70897546ed94d919dbda1b6ff5d727
jburns3141 <jburns3141@gmail.com>
2025-06-25 17:10:44 -0400

    Remove unnecessary ts code, as we are not going to support ts to json conversions and building the code.

--- specification/util/extract_values.ts
+++
@@ -1,8 +0,0 @@
-/** Utility type to recursively extract all values from the tree */
-export type ExtractValues<T> = T extends object
-  ? T[keyof T] extends infer V
-    ? V extends object
-      ? ExtractValues<V>
-      : V
-    : never
-  : never;
--- specification/util/flatten.ts
+++
@@ -1,9 +0,0 @@
-/** Flatten an object or array of strings into a single array of strings. */
-export function Flatten(objectWithStringKeys: object | string[]): string[] {
-  return Object.values(objectWithStringKeys).flatMap(value => {
-    if (typeof value === "object") {
-      return Flatten(value);
-    }
-    return value;
-  });
-}
